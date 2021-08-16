32a4dbc1e76b63b7538df870a74ff2c92234b676
StasMyronenko <stasmyronenko96@gmail.com>
2021-08-16 22:26:31 +0300

    test

--- gloves/local_settings.py
+++ gloves/local_settings.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+qwe
 SECRET_KEY = 'wr5n5je4rv3js2dv3lj#3#!3!-ewf-r2e3v5je2r35w!-23wfl235w2j34#%23fv5p325kkp'
 
 DEBUG = True
